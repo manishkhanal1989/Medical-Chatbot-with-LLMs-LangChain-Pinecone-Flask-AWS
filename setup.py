b217129fb8a29cece85f76a86508928bed273288
Manish <manishkhanal1989@gmail.com>
2025-07-19 17:18:57 -0500

    Project Structure Completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Manish Khanal",
+    author_email="manish.khanal.tech@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
